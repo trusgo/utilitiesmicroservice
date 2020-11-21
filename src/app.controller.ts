67ffb500d478d8cb09139c4234bbed5f61e51b28
vikrant <visionpreci@gmail.com>
2020-11-21 14:26:16 +0530

    added main ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,21 +6,5 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
 
-  @Get('/test')
-  testMail(@Req() req): any {
-    console.log('info', req);
-    switch (3) {
-      case 3:
-        return this.appService.getHello();
-        break;
-      default:
-        return ' No type Found';
-        break;
-    }
-  }
 }
