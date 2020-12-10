f3e3c0d44fb450438299aa9bc81a4c3ead364a09
Hikmatbki2Space <Hbk@i2space.com>
2020-12-10 12:04:13 +0530

    added qktrip icon

--- src/email/Templates/Header.ts
+++ src/email/Templates/Header.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common';
-
+import 'dotenv/config'
 @Injectable()
 export class header{
  public Template = () => {
   
     return `
-    <img style="width:60px; height: 60px; position: absolute; background-color:#0080ff" alt="qtrip logo" src="http://qktrip.i2space.in/static/media/qktrip_logo_2.f3499cdd.png"/><br/><br/>
+    <img style="width:60px; height: 60px; position: absolute" alt="qtrip logo" src=${process.env.QKTRIP_FAVICON}/><br/><br/>
     `;
   };
 }
