e85e9f4b0a1623e40aca861a6c887d6790a41f1b
Hikmatbki2Space <Hbk@i2space.com>
2021-01-25 17:17:21 +0530

    sms changes

--- src/sms/sms.service.ts
+++ src/sms/sms.service.ts
@@ -57,9 +57,10 @@ export class SMSService{
             VAR2:reqBody.data.GuestName,
             VAR3:reqBody.data.SourceName,
             VAR4:reqBody.data.DestinationName,
-            VAR5:reqBody.data.BusTypeName,
-            VAR6:Seat,VAR7:reqBody.data.Fares,
-            VAR8:Date
+            VAR5:reqBody.data.Operator,
+            VAR6:reqBody.data.BusTypeName,
+            VAR7:Seat,VAR:reqBody.data.Fares,
+            VAR9:Date
         }
         return this.sendsmsservice.sendSMS(send)
     }
@@ -93,37 +94,77 @@ export class SMSService{
 
      async AirTicket(reqBody){
          const date = reqBody.data.bookingDate
+         const segment = reqBody.data.oneWaySegment.map(x=>{
+             return ` ${x.origin} - ${x.destination}`
+         }).join(',')
+        
          const send= {
             From:process.env.SENDER_ID,
             To:reqBody.mobile,
             TemplateName:process.env.AIRTICKET_SMS_TEMP,
             VAR1:reqBody.data.guestName,
             VAR2:reqBody.data.id,
-            VAR3:'',
-            VAR4:'',
-            VAR5:'',
-            VAR6:reqBody.data.id,
-            VAR7:reqBody.data.pnr,
-            VAR8:reqBody.data.tripType,
-            VAR9:reqBody.data.pax.adults,
-            VAR10:reqBody.data.pax.childCount,
-            VAR11:reqBody.data.pax.infantCount
+            VAR3:segment,
+            VAR4:reqBody.data.oneWaySegment[0].departureDateTime,
+            VAR5:reqBody.data.id,
+            VAR6:reqBody.data.pnr,
+            VAR7:reqBody.data.tripType,
+            VAR8:reqBody.data.pax.adults,
+            VAR9:reqBody.data.pax.childCount,
+            VAR10:reqBody.data.pax.infantCount
          }
          return this.sendsmsservice.sendSMS(send)
      }
-
+     async RoundTripAirTicket(reqBody){
+        const date = reqBody.data.bookingDate
+        const onewaysegment = reqBody.data.oneWaySegment.map(x=>{
+            return ` ${x.origin} - ${x.destination}`
+        }).join(',')
+        const Twowaysegment = reqBody.data.returnSegment.map(x=>{
+            return ` ${x.origin} - ${x.destination}`
+        }).join(',')
+       
+        const send= {
+           From:process.env.SENDER_ID,
+           To:reqBody.mobile,
+           TemplateName:process.env.ROUNDTRIPAIR_SMS_TEMP,
+           VAR1:reqBody.data.guestName,
+           VAR2:reqBody.data.id,
+           VAR3:onewaysegment,
+           VAR4:reqBody.data.oneWaySegment[0].departureDateTime,
+           VAR5:Twowaysegment,
+           VAR6:reqBody.data.returnSegment[0].departureDateTime,
+           VAR7:reqBody.data.pnr,
+           VAR8:reqBody.data.tripType,
+           VAR9:reqBody.data.pax.adults,
+           VAR10:reqBody.data.pax.childCount,
+           VAR11:reqBody.data.pax.infantCount
+        }
+        return this.sendsmsservice.sendSMS(send)
+    }
      async BsTicket(reqBody){
+         
+          var name = reqBody.data.passengerInfo.map(x=>{
+              return ` ${x.titles} ${x.names}`
+          }).join(',')
+          var seat = reqBody.data.passengerInfo.map(x=>{
+              return ` ${x.seatNos}(${x.genders})`
+          }).join(',')
+         var fares = reqBody.data.passengerInfo.map(x=>{
+             return ` ${parseFloat(x.fares) + parseFloat(x.serviceTax)+  parseFloat(x.serviceCharge)}`
+         }).join(',')
               const send={
                 From:process.env.SENDER_ID,
                 To:reqBody.mobile,
                 TemplateName:process.env.BUSBOOK_SMS_TEMP,
                 VAR1:reqBody.data.pnr,
-                VAR2:'',
+                VAR2:name,
                 VAR3:reqBody.data.sourcename,
-                VAR4:reqBody.data.destinationname,
-                VAR5:reqBody.data.busTypeName,
-                VAR6:reqBody.data.noOfSeats,VAR7:reqBody.data.collectedFare,
-                VAR8:reqBody.data.journeydate
+                VAR4:reqBody.data.destinationname, 
+                VAR5:reqBody.data.operator,
+                VAR6:reqBody.data.busTypeName,
+                VAR7:seat,VAR8:fares,
+                VAR9:reqBody.data.journeydate
               }
               return this.sendsmsservice.sendSMS(send)
      }
